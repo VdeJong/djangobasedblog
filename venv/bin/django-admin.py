5e989de7f6e66b4d1a715593b30b77a072a3054d
VdeJong <vincentj@mbdev078.fab.local>
2016-09-01 10:31:52 +0200

    Remove django directory from project.

--- venv/bin/django-admin.py
+++
@@ -1,6 +0,0 @@
-#!/Users/vincentj/projects/myblog/djangobasedblog/venv/bin/python
-# EASY-INSTALL-DEV-SCRIPT: 'Django==1.11.dev20160831070041','django-admin.py'
-__requires__ = 'Django==1.11.dev20160831070041'
-__import__('pkg_resources').require('Django==1.11.dev20160831070041')
-__file__ = '/Users/vincentj/projects/myblog/djangobasedblog/django/django/bin/django-admin.py'
-exec(compile(open(__file__).read(), __file__, 'exec'))
